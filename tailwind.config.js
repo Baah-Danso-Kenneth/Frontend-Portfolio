18b8a564b9cd2536d7dab1e4bef0068eb6d0bb41
HighestDev <kenneth.danso@amalitech.org>
2023-04-29 03:19:38 +0000

    configuring tailwind to function as intended

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const {fontFamily}=require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -7,11 +9,23 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily:{
+        mont:['var(--font-mont)',...fontFamily.sans]
       },
+      
+      colors:{
+        dark:"#1b1b1b",
+        light:"#f5f5f5",
+        primary:"#b63e96",
+        primaryDark:"#58e6d9",
+      },
+      animation:{
+        'spin-slow':'spin 3s linear infinite',
+      },
+      backgroundImage:{
+        circularLight:'repeating-radial-gradient(rgba(0,0,0,0.4) 2px,#f5f5f5 5px ,#f5f5f5 80px);'
+      }
+
     },
   },
   plugins: [],
