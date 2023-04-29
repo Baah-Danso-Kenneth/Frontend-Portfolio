606807b260319b00d40085e2d144e818e0c4cb1c
HighestDev <kenneth.danso@amalitech.org>
2023-04-29 21:36:03 +0000

    pages added

---
+++ src/Types/types.ts
@@ -0,0 +1,22 @@
+import { ReactNode } from "react";
+
+export interface CustomizeLinksProps{
+    href:string;
+    className?:string | null | undefined;
+    title:string
+}
+
+export interface LayoutProps{
+    children:ReactNode
+    className?:string
+}
+
+export interface AnimatedTextProps{
+    className?:string;
+    text:string;
+}
+
+export interface IconProps{
+    className?:string | null | undefined;
+    set:[string]
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,27 @@
+import Footer from '@/components/Footer'
+import NavBar from '@/components/NavBar'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import {Montserrat} from 'next/font/google'
+import Head from 'next/head'
+
+const montserrat=Montserrat({
+   subsets:['latin'],
+   variable:"--font-mont"
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return(
+    <main className={`${montserrat.variable} font-mont bg-light w-full min-h-screen` }>
+      <Head>
+        <title>Portfolio</title>
+        <meta name="description" content='Creating a elegant portfolio' />
+        <meta name='viewport' content='width=device-width, initial-scale=1'/>
+        <link rel="icon" href='/favicon.ico' />
+      </Head>
+      <NavBar/>
+       <Component {...pageProps} />
+       <Footer/>
+    </main>
+    )
 }
